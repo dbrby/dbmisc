e39c0143610e3196f715c5f3d0d79a04ce18627a
dbraby <daniel.braby@strath.ac.uky>
2022-04-08 17:18:36 +0100

    v1

---
+++ R/caption_db.R
@@ -0,0 +1,8 @@
+#' My caption for plots
+#' 
+#' This saves me writing one line of code, everytime.
+#' @export
+#' 
+#' 
+
+caption_db <- labs(caption = "Forest Research\n Daniel Braby\n Higher Statistical Officer")
