73493d94f8581151c34fe2975ad46ae0b7b3fd76
dbraby <daniel.braby@strath.ac.uky>
2022-04-08 17:11:33 +0100

    2

--- R/add_logo.R
+++ R/add_logo.R
@@ -10,7 +10,7 @@
 add_logo <- function() {
   logo <- image_read("https://github.com/dbrby/dbmisc/blob/master/img/fr_logo2.png?raw=true")
   
-  grid::grid.raster(logo, x =, y = -0.03, just = c('left', 'bottom'), 
+  grid::grid.raster(logo, x = 0, y = -0.03, just = c('left', 'bottom'), 
                     width = unit(1, 'inches'))
 
   
