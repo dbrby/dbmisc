78c3fb2c109ba0d429c532b09ff6a778e273c772
dbraby <daniel.braby@strath.ac.uky>
2022-04-08 16:25:14 +0100

    v1

---
+++ R/add_logo.R
@@ -0,0 +1,15 @@
+#' Add Forest Research logo to ggplot2 graphics.
+#'
+#' Use magick to add logo to a data viz
+#'
+#'@importFrom magick image_read
+
+
+add_logo <- function() {
+  logo <- image_read("https://github.com/dbrby/dbmisc/blob/master/img/fr_logo2.png?raw=true")
+  
+  grid::grid.raster(logo, x = 0.09, y = -0.025, just = c('left', 'bottom'), width = unit(2, 'inches'))
+
+  
+  
+}
---
+++ R/theme_forest.R
@@ -0,0 +1,26 @@
+#' Custom ggplot Theme in Forest Research Flavor.
+#'
+#' This is a minimal change to the minimal theme of ggplot2, to position text elements and add some colour.
+#'
+
+theme_forest <- function() {
+  theme_minimal() %+replace%
+    
+    theme(
+      
+      plot.title = element_text(size = 20,
+                                face = 'bold',
+                                hjust = 0,
+                                vjust = 2,
+                                colour = forestgreen
+    ),
+      plot.subtitle = element_text(size = 14,
+                                   hjust = 0,
+                                   colour = olivedrab),
+      plot.caption = element_text(size = 9,
+                                hjust = 1,
+                                colour = darkslateblue))
+    
+    
+    
+}
